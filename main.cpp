4f3e6d9c956cf608252ec79f165fc7edb4b72f44
NXTID <117934144+NXTID@users.noreply.github.com>
2022-11-10 18:06:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
+  float a=0;
+  float b=0;
+  float c=0;
+  float quadrato=0;
+  float trapezio=0;
+  float rettangolo=0;
+  float triangolo=0;
 
-cin >> a >> b >> c;
+  cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
+  quadrato=a*a;
+  triangolo=(a*b)/2;
+  rettangolo=a*b;
+  trapezio=((a+b)*c)/2;
 
+  cout<<"\nArea quadrato= "<<quadrato;
+  cout<<"\nArea rettangolo= "<<rettangolo;
+  cout<<"\nArea triangolo= "<<triangolo;
+  cout<<"\nArea trapezio= "<<trapezio;
 
+  return 0;
 }
